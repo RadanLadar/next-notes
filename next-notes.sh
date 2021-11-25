12aa5b13d6d1c97b79c021c7df62f21ede0c1fb6
mamonarchu <radanladar@protonmail.com>
2021-11-25 17:01:51 +0100

    base features added

--- next-notes.sh
+++ next-notes.sh
@@ -5,25 +5,24 @@
 
 
 ### wishlist
-#get user prompt to create new notes
-#allow to write notes vimlike
-#create a db directory
-#create a _id
-#save names into db
+# [OK] get user prompt to create new notes
+# [OK] allow to write notes vimlike
+# [OK] create a db directory
+# [OK] create a _id
+# [OK] save names into db
 #get autocompletion 
 # [OK] get timestamp
 #search into db
 
-# VARS
-# setup path var
-path=`dirname $(realpath $0)`
-# timestamp var
-ts=`date +"%Y%m%d%H%M%s"`
-# literal date var
-date=`date +"%x"`
+### VARS
+path=`dirname $(realpath $0)` # setup file path
+ts=`date +"%Y%m%d%H%M%s"` # timestamp
+date=`date +"%x"` # get formated date
+_id=$(cat $path/params/id.txt) # ID var init
+choice="" # usr prompt init
 
-# user prompt init
-choice=""
+### DIRECTORIES STRUCTURE
+mkdir -p $path/db # if doesn't exists
 
 #clear terminal when program starts
 clear
@@ -39,9 +38,21 @@ figlet notes
 read -p 'Your choice: ' choice
 
 case ${choice,,} in
+	a)
+		read -p 'name: ' name
+		((_id+=1))
+		echo $_id > $path/params/id.txt
+		title=$_id'-'${name^^}'-'$ts
+		vim $path/db/$title
+		echo $_id ${name^^} >> $path/params/names.txt
+		;;
 	q)
 		clear
 		exit
+		;;
+	reset)
+		echo "0" > $path/params/id.txt
+		rm -rf $path/db
 		;;
 	*)
 		echo -n 'Wrong choice'
