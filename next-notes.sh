5ede4537be56c4a42183d2c105882539637ce078
mamonarchu <radanladar@protonmail.com>
2021-11-25 14:43:23 +0100

    project base

--- next-notes.sh
+++ next-notes.sh
@@ -1,3 +1,56 @@
 #!/bin/bash
 
-# git connect checking
+# -------DEPENDENCIES-------
+# -figlet ; header txt
+
+
+### wishlist
+#get user prompt to create new notes
+#allow to write notes vimlike
+#create a db directory
+#create a _id
+#save names into db
+#get autocompletion 
+# [OK] get timestamp
+#search into db
+
+# VARS
+# setup path var
+path=`dirname $(realpath $0)`
+# timestamp var
+ts=`date +"%Y%m%d%H%M%s"`
+# literal date var
+date=`date +"%x"`
+
+# user prompt init
+choice=""
+
+#clear terminal when program starts
+clear
+
+# loops to avoid unwanted exits
+while [ choice != "q" ]
+do
+
+# Header
+figlet next
+figlet notes
+
+read -p 'Your choice: ' choice
+
+case ${choice,,} in
+	q)
+		clear
+		exit
+		;;
+	*)
+		echo -n 'Wrong choice'
+		;;
+esac
+
+read -p '(Press any key to continue)'
+echo $ts
+
+
+clear
+done
